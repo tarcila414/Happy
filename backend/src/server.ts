5bb71757e0d091c3270571da3150255ba63e0c8a
tarcila414 <tarcila086@gmail.com>
2020-12-03 10:44:31 -0300

    Criação do primeiro componente
    Entendendo models
    Estabelecendo rotas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,44 @@
-import express from 'express';
+import express, { response } from 'express';
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import Orphanage from './models/Orphanage';
 import './database/connection';
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
-app.get('/users', () => {});
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  console.log('================= ' + request.body.longitude);
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(200).json({ message: 'FUNFOU' });
+});
+
+app.get('/users', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
 
 app.listen(3333);
